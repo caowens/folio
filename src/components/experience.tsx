332d13e2df4de942695477a206f19b9161a18f33
caowens <caowens555@yahoo.com>
2025-11-10 04:19:27 -0800

    Make experiences list components and data

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -7,25 +7,55 @@ import {
   TabsContent,
   TabsContents,
 } from '@/components/ui/shadcn-io/tabs';
+import Image from "next/image";
+import { works, education, type Experience } from "@/constants";
+
+function ExperienceList({
+    experiences
+}: {
+    experiences: Experience[]
+}) {
+    return (
+        <ul className="ml-10 list-none border-l border-neutral-300">
+            {experiences.map((experience) => (
+                <li key={experience.title} className="relative ml-10 py-4 pr-2">
+                    <div className="absolute -left-16 top-4 flex items-center justify-center rounded-full bg-white">
+                        <Image
+                            src={experience.logo_url}
+                            alt={experience.place}
+                            width={48}
+                            height={48}
+                            className="rounded-full bg-transparent"
+                            priority
+                            style={{ objectFit: 'cover' }} 
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1 text-white">
+                        <time className="text-xs text-gray-400">{experience.dates}</time>
+                        <h2 className="font-semibold leading-none">{experience.place}</h2>
+                        <p className="text-sm text-gray-400">{experience.title}</p>
+                        <p className="text-sm">{experience.description}</p>
+                    </div>
+                </li>
+            ))}
+        </ul>
+    );
+}
 
 export default function Experience() {
     return (
         <SectionWrapper idName="Experience">
-            <Tabs defaultValue="Experience" className="w-full rounded-lg">
+            <Tabs defaultValue="work" className="w-full rounded-lg">
                 <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="account">Experience</TabsTrigger>
-                    <TabsTrigger value="password">Education</TabsTrigger>
+                    <TabsTrigger value="work">Work</TabsTrigger>
+                    <TabsTrigger value="education">Education</TabsTrigger>
                 </TabsList>
-                <TabsContents className="mx-1 mb-1 -mt-2 rounded-sm h-full border-chart-1">
-                    <TabsContent value="account" className="space-y-6 p-6">
-                    <p className="text-sm text-muted-foreground">
-                        Make changes to your account here. Click save when you&apos;re done.
-                    </p>
+                <TabsContents className="mx-1 mb-1 -mt-2 rounded-sm h-full border-[1px] border-white">
+                    <TabsContent value="work" className="space-y-6 p-6">
+                        <ExperienceList experiences={works} />
                     </TabsContent>
-                    <TabsContent value="password" className="space-y-6 p-6">
-                    <p className="text-sm text-muted-foreground">
-                        Change your password here. After saving, you&apos;ll be logged out.
-                    </p>
+                    <TabsContent value="education" className="space-y-6 p-6">
+                        <ExperienceList experiences={education} />
                     </TabsContent>
                 </TabsContents>
             </Tabs>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,6 +9,14 @@ interface Contact {
   hoverColor: string;
 }
 
+export interface Experience {
+  title: string,
+  place: string,
+  dates: string,
+  logo_url: string,
+  description?: string,
+}
+
 export const contacts: Contact[] = [
   {
     method: "LinkedIn",
@@ -38,4 +46,44 @@ export const contacts: Contact[] = [
     icon: Mail,
     hoverColor: "bg-orange-200 dark:bg-orange-700",
   },
+];
+
+export const works: Experience[] = [
+  {
+    title: "Software Engineer 1",
+    place: "Microsoft",
+    dates: "Aug 2024 - Aug 2025",
+    logo_url: "/works/microsoft.png",
+    description: "Implemented automated UI and end-to-end tests to improve deployment confidence and service reliability, while optimizing CI/CD pipelines to cut deployment time by 77%. Led cross-team projects for telemetry and reporting, managed on-call operations, and mentored new hires to increase team efficiency."
+  },
+  {
+    title: "Software Engineer Intern",
+    place: "Microsoft",
+    dates: "May 2023 - Aug 2023",
+    logo_url: "/works/microsoft.png",
+    description: "Enhanced a C# virtual file system and automated data collection with PowerShell scripts, providing insights that prevented costly adoption risks and informed infrastructure decisions."
+  },
+  {
+    title: "Explore Intern (SWE + PM)",
+    place: "Microsoft",
+    dates: "May 2022 - Aug 2022",
+    logo_url: "/works/microsoft.png",
+    description: "Developed an anti-phishing automation pipeline in C# and Azure, improving Microsoft Edge security, while coordinating with engineers and PMs to strengthen project alignment and deliverables."
+  },
+  {
+    title: "STEP Intern (SWE)",
+    place: "Google",
+    dates: "May 2021 - Aug 2021",
+    logo_url: "/works/new-google.png",
+    description: "Built and refactored Java testing infrastructure to improve Nest thermostat reliability and test maintainability, contributing to stronger team alignment and higher system quality."
+  },
+];
+
+export const education: Experience[] = [
+  {
+    title: "B.Sc. in Computer Science",
+    place: "Fisk University",
+    dates: "Aug 2020 - May 2024",
+    logo_url: "/education/fisk-modern.png",
+  }
 ];
