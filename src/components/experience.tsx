c36151ae8513e97320cea803819af114b029d7d4
caowens <caowens555@yahoo.com>
2025-11-10 02:51:20 -0800

    Add sliding tabs effect to experience section

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -1,9 +1,34 @@
+'use client';
 import { SectionWrapper } from "./section-wrapper";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+  TabsContents,
+} from '@/components/ui/shadcn-io/tabs';
 
 export default function Experience() {
     return (
         <SectionWrapper idName="Experience">
-            <p>Work + Experience</p>
+            <Tabs defaultValue="Experience" className="w-full rounded-lg">
+                <TabsList className="grid w-full grid-cols-2">
+                    <TabsTrigger value="account">Experience</TabsTrigger>
+                    <TabsTrigger value="password">Education</TabsTrigger>
+                </TabsList>
+                <TabsContents className="mx-1 mb-1 -mt-2 rounded-sm h-full border-chart-1">
+                    <TabsContent value="account" className="space-y-6 p-6">
+                    <p className="text-sm text-muted-foreground">
+                        Make changes to your account here. Click save when you&apos;re done.
+                    </p>
+                    </TabsContent>
+                    <TabsContent value="password" className="space-y-6 p-6">
+                    <p className="text-sm text-muted-foreground">
+                        Change your password here. After saving, you&apos;ll be logged out.
+                    </p>
+                    </TabsContent>
+                </TabsContents>
+            </Tabs>
         </SectionWrapper>
     );
 };
