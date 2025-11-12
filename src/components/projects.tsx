b79f74bfef1275728d13011614998e652fee9572
caowens <caowens555@yahoo.com>
2025-11-11 23:48:23 -0800

    Make sure cursor is active when hovering over discover button

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -27,7 +27,7 @@ const ProjectCard = ({
                     <h3 className="text-lg text-gray-50">
                         {project.name}
                     </h3>
-                    <button className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 relative group overflow-hidden bg-transparent text-gray-50 rounded-full hover:bg-transparent hover:text-gray-50 transition-transform duration-500 hover:scale-105">
+                    <button className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input h-10 px-4 py-2 relative group overflow-hidden bg-transparent text-gray-50 rounded-full hover:bg-transparent hover:text-gray-50 transition-transform duration-500 hover:scale-105 cursor-pointer">
                         <span>Discover</span>
                         <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white to-transparent opacity-30 transform -translate-x-full group-hover:translate-x-full transition duration-700"></div>
                     </button>
