f7e435aa4f9e7c559170d252d65bf64eef4e5e1d
caowens <caowens555@yahoo.com>
2025-11-11 22:54:43 -0800

    Add View More button

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -2,7 +2,7 @@ import { SectionWrapper } from "./section-wrapper";
 
 export default function Projects() {
     return (
-        <SectionWrapper idName="Projects">
+        <SectionWrapper idName="Projects" viewMoreLink={{href: "/projects"}}>
             <p>Projects</p>
         </SectionWrapper>
     );
