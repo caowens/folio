a7952af10c9fca951db57e599ac1ff8b4104d00f
caowens <caowens555@yahoo.com>
2025-11-14 18:10:43 -0800

    Upper case projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import StandardPageLayout from "@/components/standard-page-layout";
 export default function ProjectsPage() {
     return (
         <StandardPageLayout
-            title="Projects"
+            title="PROJECTS"
             description="Explore a list of projects showcasing various skills and technologies."
         >
             <FeaturedProjects isFullList={true} /> 
